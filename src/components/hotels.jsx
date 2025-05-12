c3eb4ec2a08131844e0b65991f6f224b4f6ce2c9
Diremu Adebanjo <diremuadebanjo@gmail.com>
2025-05-12 11:22:46 +0100

    changed title: hotels

--- src/components/hotels.jsx
+++ src/components/hotels.jsx
@@ -5,8 +5,8 @@ export default function Hotels() {
     const hotelList = Properties.hotels
     return (
         <div>
-            <h2 className="text-3xl text-[#f9fafb] font-semibold bg-[url('/assets/images/apartment-title.jpg')] mt-6 bg-cover bg-center py-10">Apartments</h2>
-            <p className="text-[16px] italic">Our Highly Rated Apartments</p>
+            <h2 className="text-3xl text-[#f9fafb] font-semibold bg-[url('/assets/images/apartment-title.jpg')] mt-6 bg-cover bg-center py-10">Hotels</h2>
+            <p className="text-[16px] italic">Our Prestigious Hotels</p>
             <div className="flex flex-wrap justify-center">
                 <ShowcaseBelt properties={hotelList} type="hotels" />
             </div>
