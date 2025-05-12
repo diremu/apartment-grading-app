44c007eea382f3b943e324cb976243ded1540cf5
Diremu Adebanjo <diremuadebanjo@gmail.com>
2025-05-12 14:24:44 +0100

    added title images for hotels and condos

--- src/components/condos.jsx
+++ src/components/condos.jsx
@@ -5,7 +5,7 @@ export default function Condos() {
     const condoList = Properties.condos
     return (
         <div>
-            <h2 className="text-3xl text-[#f9fafb] font-semibold bg-[url('/assets/images/apartment-title.jpg')] mt-6 bg-cover bg-center py-10">Condos</h2>
+            <h2 className="text-3xl text-[#f9fafb] font-semibold bg-[url('/assets/images/condo-banner.jpg')] mt-6 bg-cover bg-center py-10">Condos</h2>
             <p className="text-[16px] italic">Our Comfy Condos</p>
             <div className="flex flex-wrap justify-center">
                 <ShowcaseBelt properties={condoList} type="condos" />
--- src/components/hotels.jsx
+++ src/components/hotels.jsx
@@ -5,7 +5,7 @@ export default function Hotels() {
     const hotelList = Properties.hotels
     return (
         <div>
-            <h2 className="text-3xl text-[#f9fafb] font-semibold bg-[url('/assets/images/apartment-title.jpg')] mt-6 bg-cover bg-center py-10">Hotels</h2>
+            <h2 className="text-3xl text-[#f9fafb] font-semibold bg-[url('/assets/images/hotel-banner.jpg')] mt-6 bg-cover bg-center py-10">Hotels</h2>
             <p className="text-[16px] italic">Our Prestigious Hotels</p>
             <div className="flex flex-wrap justify-center">
                 <ShowcaseBelt properties={hotelList} type="hotels" />
